5c13a6d4922b3f4a3f044438e669e5c4a6f11089
EXT. Jorge Luis Hernandez Cañon <jorge.hernandez@sanidad.mil.co>
2025-03-30 22:41:06 -0500

    Se agregan los modelos para el filtrado de las adquisiciones

---
+++ Models/FiltrosAdquisicion.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.Models
+{
+    public class FiltrosAdquisicion
+    {
+        public string? Presupuesto { get; set; }
+        public string? Unidad { get; set; }
+        public string? TipoBienServicio { get; set; }
+        public DateTime? FechaDesde { get; set; }
+        public DateTime? FechaHasta { get; set; }
+        public string? Proveedor { get; set; }
+        public int Pagina { get; set; } = 1;
+        public int ItemsPorPagina { get; set; } = 10;
+    }
+}
---
+++ Models/ResultadoPaginado.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Models
+{
+    public class ResultadoPaginado<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int Total { get; set; }
+        public int Pagina { get; set; }
+        public int ItemsPorPagina { get; set; }
+    }
+}
